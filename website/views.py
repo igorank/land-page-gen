cebab57c97ebde8051a8cbe8823284bec397234f
Igor Anikin <anikinigor09@gmail.com>
2023-01-05 15:06:13 +0200

    index.html has been changed

--- website/views.py
+++ website/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+from django.conf import settings
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'website/index.html', {})
+    context = {}
+
+    if request.method == 'POST':
+        LandingPageName = request.POST['LandingPageName']
+        LandingPageDetails = request.POST['LandingPageDetails']
+
+        print(LandingPageName)
+        print(LandingPageDetails)
+
+    return render(request, 'website/index.html', context)

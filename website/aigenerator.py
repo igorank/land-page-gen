dbfd938374d60434f353473323319ade41235658
Igor Anikin <anikinigor09@gmail.com>
2023-01-06 12:40:37 +0200

    aigenerator.py has been changed

--- website/aigenerator.py
+++ website/aigenerator.py
@@ -1,16 +1,15 @@
-from .chatgpt import ChatGPT
+import requests
 
-bot = ChatGPT()
-
+CHATGPT_API_IP = "89.252.17.107"
+CHATGPT_API_PORT = "8060"
 
 def getSection1Title(text) -> str:
     # response = bot.ask(f"Generate a website landing page title (only 5 words in the title) for the following business:\nWhat the business does: {text}")
-    response = bot.ask("Generate a website landing page title")
-    return response
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt", json={'text': "Generate a website landing page title"})
+    return response.text
 
 
 def getSection1Description(business_name, text) -> str:
     # response = bot.ask(f"Generate a website landing page description for the following business:\nBusiness Name: f{business_name}\nWhat the business does: {text}")
-    response = bot.ask("Generate a website landing page description")
-                       # f"Content length should be: between 140 to 160 words)")
-    return response
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt", json={'text': "Generate a website landing page description"})
+    return response.text

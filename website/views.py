e5296cf91a176873dc13cc15f87e1ca0704a325a
Igor Anikin <anikinigor09@gmail.com>
2023-01-06 14:18:49 +0200

    added features generation

--- website/views.py
+++ website/views.py
@@ -26,12 +26,31 @@ def home(request):
             obj['description'] = service_description
             services.append(obj)
 
+        features = []
+        features_titles = get_features(LandingPageDetails)
+        for feature in features_titles:
+            obj = {}
+            feature_description = get_service_description(feature)
+            obj['title'] = feature
+            obj['description'] = feature_description
+            features.append(obj)
+
         context['service1Title'] = services[0]['title']
         context['service1Description'] = services[0]['description']
         context['service2Title'] = services[1]['title']
         context['service2Description'] = services[1]['description']
         context['service3Title'] = services[2]['title']
         context['service3Description'] = services[2]['description']
+
+        context['section3Title'] = getSection1Title(LandingPageDetails)
+        context['section3Description'] = getSection1Description(LandingPageName, LandingPageDetails)
+
+        context['feature1Title'] = features[0]['title']
+        context['feature1Description'] = features[0]['description']
+        context['feature2Title'] = features[1]['title']
+        context['feature2Description'] = features[1]['description']
+        context['feature3Title'] = features[2]['title']
+        context['feature3Description'] = features[2]['description']
 
         return render(request, 'website/ai-website.html', context)
 

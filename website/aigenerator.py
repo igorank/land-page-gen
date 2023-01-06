3c8911b08fab55e941f106f421d39c9306fd8d48
Igor Anikin <anikinigor09@gmail.com>
2023-01-06 14:07:41 +0200

    aigenerator.py has been changed

--- website/aigenerator.py
+++ website/aigenerator.py
@@ -8,7 +8,7 @@ def getSection1Title(text) -> str:
     # response = bot.ask(f"Generate a website landing page title (only 5 words in the title) for the following business:\nWhat the business does: {text}")
     response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
                              json={
-                                 'text': f"Generate a one website landing page title (only 5 words in the title) for the following business:What the business does: {text}"})
+                                 'text': f"Generate a one website landing page title (only 5 words in the title) for the following business: What the business does: {text}"})
     return response.text.replace('"', '')
 
 
@@ -16,5 +16,41 @@ def getSection1Description(business_name, text) -> str:
     # response = bot.ask(f"Generate a website landing page description for the following business:\nBusiness Name: f{business_name}\nWhat the business does: {text}")
     response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
                              json={
-                                 'text': f"Generate a one website landing page description for the following business:Business Name: {business_name}. What the business does: {text}"})
+                                 'text': f"Generate a one website landing page description for the following business: Business Name: {business_name}. What the business does: {text}"})
+    return response.text.replace('"', '')
+
+
+def get_services(text) -> list:
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
+                             json={
+                                 'text': f"Generate 3 short and punchy website service titles for a business: What business does: {text}"})
+    answer = response.text.replace('\n', '').replace('1', '').replace('2', '').replace('3', '')
+    stripped = answer.split('"', 1)[1]
+    stripped = stripped.replace('"', '')
+    answer_list = stripped.split('.')
+    return answer_list
+
+
+def get_service_description(title) -> str:
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
+                             json={
+                                 'text': f"Generate a one description for the following service: Servie Title: {title}"})
+    return response.text.replace('"', '')
+
+
+def get_features(text) -> list:
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
+                             json={
+                                 'text': f"Generate 3 short and punchy website feature titles for a business: What business does: {text}"})
+    answer = response.text.replace('\n', '').replace('1', '').replace('2', '').replace('3', '')
+    stripped = answer.split('"', 1)[1]
+    stripped = stripped.replace('"', '')
+    answer_list = stripped.split('.')
+    return answer_list
+
+
+def get_service_description(title) -> str:
+    response = requests.post(f"http://{CHATGPT_API_IP}:{CHATGPT_API_PORT}/chatgpt",
+                             json={
+                                 'text': f"Generate a one description for the following feature: Feature Title: {title}"})
     return response.text.replace('"', '')
--- website/views.py
+++ website/views.py
@@ -17,6 +17,22 @@ def home(request):
         context['section1Title'] = getSection1Title(LandingPageDetails)
         context['section1Description'] = getSection1Description(LandingPageName, LandingPageDetails)
 
+        services = []
+        service_titles = get_services(LandingPageDetails)
+        for service in service_titles:
+            obj = {}
+            service_description = get_service_description(service)
+            obj['title'] = service
+            obj['description'] = service_description
+            services.append(obj)
+
+        context['service1Title'] = services[0]['title']
+        context['service1Description'] = services[0]['description']
+        context['service2Title'] = services[1]['title']
+        context['service2Description'] = services[1]['description']
+        context['service3Title'] = services[2]['title']
+        context['service3Description'] = services[2]['description']
+
         return render(request, 'website/ai-website.html', context)
 
     return render(request, 'website/index.html', context)
